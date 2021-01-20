687a327be5014e96ea56bafefe59b70cb7cd2f14
BianTan <biantan@outlook.com>
2021-01-20 11:38:46 +0800

    fix🐛

--- src/components/skeleton/SkeletonPost.vue
+++ src/components/skeleton/SkeletonPost.vue
@@ -13,3 +13,12 @@
     </div>
   </card>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Card from '@/components/Card.vue'
+
+export default defineComponent({
+  components: { Card }
+})
+</script>
