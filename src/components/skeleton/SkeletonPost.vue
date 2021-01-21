8ff98b57249f9e58f01e5350662b9f3c86d4e124
BianTan <biantan@outlook.com>
2021-01-21 09:04:26 +0800

    style💄 ：微微调整

--- src/components/skeleton/SkeletonPost.vue
+++ src/components/skeleton/SkeletonPost.vue
@@ -2,13 +2,13 @@
   <card class="overflow-hidden px-8 md:px-12 py-6 md:py-8">
     <div class="animate-pulse">
       <div class="flex mb-12">
-        <skeleton-card class="h-11 w-12 rounded-full mr-4" />
+        <skeleton-card class="h-10 sm:h-11 w-12 rounded-full mr-4" />
         <div class="flex flex-col justify-between w-full">
           <skeleton-card class="w-16" />
           <skeleton-card class="w-2/3" />
         </div>
       </div>
-      <skeleton-card class="w-1/3 h-8 mb-8" />
+      <skeleton-card class="w-1/3 h-6 sm:h-8 mb-8" />
       <skeleton-card class="w-5/6" />
     </div>
   </card>
