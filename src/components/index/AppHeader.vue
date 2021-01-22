3c7bf72ed0b70a58da25e5849c9c66f0f579af65
BianTan <biantan@outlook.com>
2021-01-22 20:45:45 +0800

    fix🐛

--- src/components/index/AppHeader.vue
+++ src/components/index/AppHeader.vue
@@ -1,11 +1,12 @@
 <template>
   <header class="shadow-md fixed top-0 left-0 w-full bg-white z-40 h-18" ref="mobileMenuRef">
     <menu class="flex container mx-auto my-0 px-8 h-full items-center justify-between">
-      <router-link to="/" class="font-bold">我是笨蛋小扁担</router-link>
+      <p class="font-bold select-none">我是笨蛋小扁担</p>
       <nav class="hidden lg:block space-x-6">
         <router-link :to="item.url" v-for="item in menuList" :key="item.id">{{item.title}}</router-link>
       </nav>
-      <i class="iconfont icon-search text-2xl cursor-pointer" @click="handleMenuClick" />
+      <i class="iconfont icon-search text-2xl cursor-pointer hidden lg:block" />
+      <i class="iconfont icon-menu text-2xl cursor-pointer block lg:hidden" @click="handleMenuClick"/>
     </menu>
     <transition-default>
       <menu class="lg:hidden bg-white fixed top-18 my-0 w-full z-50 shadow pl-0" v-show="isShow">
@@ -21,50 +22,52 @@
 import { defineComponent, toRefs, watch, reactive, ref } from 'vue'
 import useClickOutside from '@/utlis/useClickOutside'
 import TransitionDefault from '../transition/TransitionDefault.vue'
-const menuList = [
-  {
-    id: 0,
-    url: '/',
-    title: 'Home'
-  },
-  {
-    id: 1,
-    url: '/about',
-    title: 'About'
-  },
-  {
-    id: 2,
-    url: '/themes',
-    title: 'Themes'
-  },
-  {
-    id: 3,
-    url: '/guestbook',
-    title: 'GuestBook'
-  },
-  {
-    id: 4,
-    url: '/myfriends',
-    title: 'MyFriends'
-  }
-]
 
 export default defineComponent({
   components: {
     TransitionDefault
   },
   setup() {
+    const menuList = [
+      {
+        id: 0,
+        url: '/',
+        title: 'Home'
+      },
+      {
+        id: 1,
+        url: '/about',
+        title: 'About'
+      },
+      {
+        id: 2,
+        url: '/themes',
+        title: 'Themes'
+      },
+      {
+        id: 3,
+        url: '/guestbook',
+        title: 'GuestBook'
+      },
+      {
+        id: 4,
+        url: '/myfriends',
+        title: 'MyFriends'
+      }
+    ]
+
+    const mobileMenuRef = ref<null | HTMLElement>(null) // 手机版面 Menu 的 refs
+    const outsideIsClick = useClickOutside(mobileMenuRef) // 是否点击了手机版面 Menu 的外面
     const mobileMenu = reactive({
       isShow: false,  // 控制手机版面的 Menu 是否显示
       handleMenuClick: () => {  // 切换显示状态
         mobileMenu.isShow = !mobileMenu.isShow
       }
     })
-    const mobileMenuRef = ref<null | HTMLElement>(null) // 手机版面 Menu 的 refs
-    const outsideIsClick = useClickOutside(mobileMenuRef) // 是否点击了手机版面 Menu 的外面
     watch(outsideIsClick, () => { // 状态发生改变
-      if (outsideIsClick && mobileMenu.isShow) {  // 如果点击了外面并且 Menu 打开了
-        mobileMenu.isShow = outsideIsClick.value = false  // 关闭手机 Menu
+      if (outsideIsClick.value) {  // 如果点击了外面
+        // outsideIsClick.value = false
+        if(mobileMenu.isShow) mobileMenu.isShow = false  // 如果 Menu 打开了，就关闭 Menu
       }
     })
     return {
