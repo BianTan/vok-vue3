57ceebe2ec23fa84d02d04c6ae15202b3e6a9f3c
BianTan <biantan@outlook.com>
2021-04-28 18:03:51 +0800

    perf👌

--- src/libs/VueLightbox/directives/index.ts
+++ src/libs/VueLightbox/directives/index.ts
@@ -10,7 +10,6 @@ interface OptionProps {
 
 export default {
   mounted(el: HTMLElement, binding?: BindingProps) {
-    console.log(binding.value)
     const _ops: OptionProps | null = binding.value
     const imgAry: string[] = []
     const allImg: NodeList = el.querySelectorAll('img')
@@ -18,11 +17,14 @@ export default {
     allImg.forEach((img: any, index: number) => {
       const href: string = img.parentNode.href  // 获取父节点的href
       if (!href) {  // 不存在说明img没有a标签包裹
-        const aNode = document.createElement('a') // 创建a标签
-        aNode.href = img.src  // 设置href为图片src
-        aNode.setAttribute('data-lightbox', index.toString()) // 设置当前图片的id
-        img.parentNode.replaceChild(aNode, img)  // 将img替换成创建的a标签
-        aNode.append(img) // 创建的a标签内插入原先的img
+        // const aNode = document.createElement('a') // 创建a标签
+        // aNode.href = img.src  // 设置href为图片src
+        // aNode.setAttribute('data-lightbox', index.toString()) // 设置当前图片的id
+        // img.parentNode.replaceChild(aNode, img)  // 将img替换成创建的a标签
+        // aNode.append(img) // 创建的a标签内插入原先的img
+        const temp = img.outerHTML
+        const template = `<a href="${img.src}" data-lightbox="${index}">${temp}</a>`
+        img.outerHTML = template
       } else {
         img.parentNode.setAttribute('data-lightbox', index.toString())
       }
@@ -37,10 +39,9 @@ export default {
 
     el.addEventListener('click', (e: any) => {
       const target: any = e.target
-      const res = [].includes.call(allImg, target)
-      if(res) {
+      const id: string = target.parentNode.getAttribute('data-lightbox')  // 获取当前点击图片父元素a的data-lightbox属性
+      if(id) {
         e.preventDefault()  // 阻止默认事件
-        const id: string = target.parentNode.getAttribute('data-lightbox')  // 获取当前点击图片的id
         if(_ops && _ops.openLightbox) _ops.openLightbox(id) // 回调函数，使用图片暗箱展示当前点击的图片
       }
     })
